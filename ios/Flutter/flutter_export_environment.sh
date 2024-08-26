c484fc8c772fd0f8c0a54ec473a51060cb66bdd9
Tiago Silva <devtappsinc@gmail.com>
2024-08-26 08:14:03 -0300

    fix

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/tiago/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/tiago/Documentos/Projetos/plugins/flutter_map_astp"
+export "FLUTTER_ROOT=/home/tiago/Programas/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/tiago/Documentos/libs/flutter_map_atsp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.6"
+export "FLUTTER_BUILD_NUMBER=0.0.6"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
